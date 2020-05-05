ca2b843c2d5e867d0f5c35d00427bf7e4540f4b3
kallal103 <kallal.nsu103@gmail.com>
2020-05-06 03:38:39 +0600

    Chapter 28

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -39,7 +39,7 @@ trait RecordsActivity
      */
     protected function activityDescription($description)
     {
-        return "{$description}_" . strtolower(class_basename($this));
+        return "{$description}_" . strtolower(class_basename($this));//created_task
     }
 
     /**
@@ -64,11 +64,14 @@ trait RecordsActivity
     public function recordActivity($description)
     {
         $this->activity()->create([
+            'user_id'=> ($this->project ?? $this)->owner->id,
             'description' => $description,
             'changes' => $this->activityChanges(),
             'project_id' => class_basename($this) === 'Project' ? $this->id : $this->project_id
         ]);
     }
+
+  
 
     /**
      * The activity feed for the project.
