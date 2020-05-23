a88849858db6080a20168e2b4621d8a440fb53d4
kallal103 <kallal.nsu103@gmail.com>
2020-05-24 05:56:35 +0600

    Episode 33

--- tests/Feature/InvitationsTest.php
+++ tests/Feature/InvitationsTest.php
@@ -11,18 +11,54 @@ class InvitationsTest extends TestCase
 {
     use RefreshDatabase;
 
-    function test_a_project_can_invite_a_user(){
+    function test_non_owners_may_not_invite_users()
+    {
+        $this->actingAs(factory(User::class)->create())
+            ->post(ProjectFactory::create()->path() . '/invitations')
+            ->assertStatus(403);
+
+    }
+
+    function test_a_project_owner_can_invite_a_user()
+    {
+        $project = ProjectFactory::create();
+
+        $userToInvite = factory(User::class)->create();
+
+        $this->actingAs($project->owner)
+            ->post($project->path() . '/invitations', [
+                'email' => $userToInvite->email
+            ])
+            ->assertRedirect($project->path());
+
+        $this->assertTrue($project->members->contains($userToInvite));
+    }
+
+    function test_the_email_address_must_be_associated_with_a_valid_birdboard_account()
+    {
+        $project = ProjectFactory::create();
+        $this->actingAs($project->owner)
+            ->post($project->path() . '/invitations', [
+                'email' => 'notauser@example.com'
+            ])
+            ->assertSessionHasErrors([
+                'email' => 'The user you are inviting must have a Birdboard account.'
+            ]);
+    }
+
+    function test_invited_users_may_update_project_details()
+    {
         //Given I have a project
         $project = ProjectFactory::create();
 
         //and the owner of the project invites another user
-        
+
         $project->invite($newUser = factory(User::class)->create());
 
         //Then the new user will have permission to add tasks
 
-        $this->signIn($newUser);
-        $this->post(action('ProjectTasksController@store',$project), $task = ['body'=>'foo task']);
+        $this->actingAs($newUser);
+        $this->post(action('ProjectTasksController@store', $project), $task = ['body' => 'foo task']);
 
         $this->assertDatabaseHas('tasks', $task);
     }
