165e56c6dcff09d0bf0ec9bc0f1c1ee6b9820a61
kallal103 <kallal.nsu103@gmail.com>
2020-05-27 22:17:41 +0600

    Tailwind error

--- public/js/app.js
+++ public/js/app.js
@@ -49924,7 +49924,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/src/index.js):\nSyntaxError\n\n(1:11) `@tailwind preflight` is not a valid at-rule in Tailwind v1.0, use `@tailwind base` instead.\n\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 1 | \u001b[39m\u001b[36m@tailwind\u001b[39m preflight\u001b[33m;\u001b[39m\n \u001b[90m   | \u001b[39m          \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 2 | \u001b[39m\u001b[36m@tailwind\u001b[39m components\u001b[33m;\u001b[39m\n \u001b[90m 3 | \u001b[39m\u001b[33m.button\u001b[39m \u001b[33m{\u001b[39m\n\n    at C:\\laragon\\www\\birdboard\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\laragon\\www\\birdboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\laragon\\www\\birdboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:233:18\n    at context.callback (C:\\laragon\\www\\birdboard\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at C:\\laragon\\www\\birdboard\\node_modules\\postcss-loader\\src\\index.js:207:9");
 
 /***/ }),
 
