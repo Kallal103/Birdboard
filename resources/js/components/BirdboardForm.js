3ba5c6dd0629d6e2d7b42a6c5ff5adb6c3680812
kallal103 <kallal.nsu103@gmail.com>
2020-06-10 13:30:51 +0600

    Episode 42

---
+++ resources/js/components/BirdboardForm.js
@@ -0,0 +1,62 @@
+import Axios from "axios";
+
+class BirdboardForm{
+    constructor(data){
+        //this.data = data;
+
+        //this.originalData = {}
+
+        this.originalData = JSON.parse(JSON.stringify(data));
+
+        Object.assign(this, data);
+
+        this.errors = {};
+
+        this.submitted = false;
+    }
+
+    data(){
+        return Object.keys(this.originalData).reduce((data, attribute) => {
+            data[attribute] = this[attribute];
+
+            return data;
+        }, {});
+
+    }
+    post(endpoint){
+        this.submit(endpoint);
+
+    }
+
+    patch(endpoint){
+        this.submit(endpoint, 'patch');
+    }
+    
+    delete(endpoint){
+        this.submit(endpoint, 'delete');
+    }
+    submit(endpoint, requestType= 'post'){
+       return axios[requestType](endpoint, this.data())
+       .catch(this.onFail.bind(this))
+       .then(this.onSucess.bind(this));
+    }
+
+    onSucess(response){
+        this.submitted = true;
+        this.errors = {};
+        return response;
+    }
+
+    onFail(error){
+        this.errors = error.response.data.errors;
+        this.submitted = false;
+
+        throw error;
+    }
+
+    reset(){
+        Object.assign(this, this.originalData);
+    }
+}
+
+export default BirdboardForm;
